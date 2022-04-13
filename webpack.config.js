67b3f7ee7db513ce23cee0633e82c621908585da
v_lialwang(王亮) <v_lialwang@tencent.com>
2022-04-13 18:34:37 +0800

    fix:webpack入口路径配置错误

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production', // 模式
-    entry: './src/main.js', // 打包入口地址
+    entry: './main.js', // 打包入口地址
     output: {
         filename: 'bundle.js', // 输出文件名
         path: path.join(__dirname, 'dist'), // 输出文件目录
@@ -13,7 +13,6 @@ module.exports = {
             // 转换规则
             {
                 test: /\.less$/, //匹配所有的 less 文件
-                use: 'css-loader', // use: 对应的 Loader 名称
                 use: ['style-loader', 'css-loader', 'less-loader'], // use: 对应的 Loader 名称
             },
         ],
