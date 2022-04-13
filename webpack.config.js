18c0da290a098f75428e058701cf0d782819b296
v_lialwang(王亮) <v_lialwang@tencent.com>
2022-04-13 14:35:07 +0800

    feature:webpack less demo

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production', // 模式
+    entry: './src/main.js', // 打包入口地址
+    output: {
+        filename: 'bundle.js', // 输出文件名
+        path: path.join(__dirname, 'dist'), // 输出文件目录
+    },
+    module: {
+        rules: [
+            // 转换规则
+            {
+                test: /\.less$/, //匹配所有的 less 文件
+                use: 'css-loader', // use: 对应的 Loader 名称
+                use: ['style-loader', 'css-loader', 'less-loader'], // use: 对应的 Loader 名称
+            },
+        ],
+    },
+    plugins: [
+        // 配置插件
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'dist'), // 静态文件目录
+        compress: true, //是否启动压缩 gzip
+        port: 8080, // 端口号
+        open: true, // 是否自动打开浏览器
+    },
+};
